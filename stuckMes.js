da113bcc7d4873aee08f9562dbdd5608b20b82a9
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-07-05 18:03:02 +0300

    Update stuckMes.js

--- stuckMes.js
+++ stuckMes.js
@@ -503,7 +503,9 @@ function fixFirstName(phoneNum) {
     splittedName[0] === "דוקטור" ||
     splittedName[0] === "פרופסור" ||
     splittedName[0] === "פרופ'" ||
-    splittedName[0] === "Dr."
+    splittedName[0] === "Dr."||
+     splittedName[0] === "הרב" ||
+     splittedName[0] === "ד״ר" 
   ) {
     return splittedName[1];
   }
@@ -646,7 +648,9 @@ function fixCreatorFirstName() {
     splittedName[0] === "דוקטור" ||
     splittedName[0] === "פרופסור" ||
     splittedName[0] === "פרופ'" ||
-    splittedName[0] === "Dr."
+    splittedName[0] === "Dr."||
+     splittedName[0] === "הרב" ||
+     splittedName[0] === "ד״ר" 
   ) {
     return splittedName[1];
   }
