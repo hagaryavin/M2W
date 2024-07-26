6494db48fd3029dde47efda02ba2c0f839f59348
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-07-26 14:37:37 +0300

    Update postMesEng.js

--- postMesEng.js
+++ postMesEng.js
@@ -1,1 +1,417 @@
+var options = document.getElementById("people");
+var personOption;
+var allPeople = [];
+var rowCount = 2;
+var size = 0;
+var fullName = "";
+var firstName = "";
+var optionsCrew = document.getElementById("crew");
+var crewOption;
+var crewList = [];
+var currCrew = {};
+var newCrewMem;
+var messes = [
+  { name: "", lines: [] },
+  { name: "", lines: [] },
+  { name: "", lines: [] },
+  { name: "", lines: [] },
+  { name: "", lines: [] },
+  { name: "", lines: [] },
+    { name: "", lines: [] }  ,
+    { name: "", lines: [] }  ,
+    { name: "", lines: [] }  
+];
+var fullTexts = [[], [], [], [], [], [],[],[],[]];
+var wannaFixGuestPhone = true;
+const url =
+  "https://script.google.com/macros/s/AKfycbwif1D1ZdoI1iYaL2Hya5Jke8UIFaoPxMo2Jkvd3cNytK35UIGbJZ0NKwhiYJQgana8-A/exec";
+var newPerson = {};
+var chainOption;
+var allChains = [];
+var newChain = {};
+var currChain = {};
+var chainDataURL =
+  "https://script.google.com/macros/s/AKfycbyEekfuBfk9W8aWqk9_uOa-Imynp5d3kKHjAebD6WuL-e7d2xN8RdBRsPefUJWcflgMsQ/exec";
+getChainData();
+getCrewData();
+getData();
+function getData() {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      json.data.forEach((ele) => {
+        newPerson = {
+          name: ele.name,
+          phone: ele.phone,
+          chain: ele.chain,
+          linkfive: ele.linkfive,
+          linkshort: ele.linkshort,
+          linkshortyt: ele.linkshortyt,
+          linkfull: ele.linkfull,
+          linkspotify: ele.linkspotify,
+          row: rowCount,
+        };
+        if (ele.fixedname !== "") newPerson.name = ele.fixedname;
+        if (ele.fixedphone !== "") newPerson.phone = ele.fixedphone;
+        if (ele.fixedchain !== "") newPerson.chain = ele.fixedchain;
+        allPeople.push(newPerson);
+        console.log(allPeople[size]);
+        personOption = document.createElement("option");
+        personOption.value =
+          newPerson.name + " + " + fixChainFromData(newPerson.chain);
+        personOption.id = rowCount;
+        if (newPerson.name !== "" || newPerson.chain !== "") {
+          options.append(personOption);
+        }
+        rowCount++;
+        size++;
+      });
+    });
+}
+function getChainData() {
+  fetch(chainDataURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      json.data.chains.forEach((ele) => {
+        newChain = {
+          name: ele.name,
+          altName: ele.othername,
+          playlist: ele.playlist,
+          description: ele.description,
+        };
+        allChains.push(newChain);
+        chainOption = document.createElement("option");
+        chainOption.value = newChain.name;
+        document.getElementById("chainsNames").append(chainOption);
+      });
+    });
+}
+function getCrewData() {
+  fetch(chainDataURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      json.data.crew.forEach((ele) => {
+        newCrewMem = {
+          name: ele.name,
+          phone: ele.phone,
+        };
+        crewList.push(newCrewMem);
+        crewOption = document.createElement("option");
+        crewOption.value = newCrewMem.name;
+        optionsCrew.append(crewOption);
+        if(newCrewMem.name==="יעל"){
+            document.getElementById("crewList").value = "יעל"; 
+        }  
+      });
+    });
+}
+function getMessData() {
+  var newMess;
+  var messes1 = [];
+  var messes2 = [];
+  fetch(chainDataURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      json.data.messagesEng.forEach((ele) => {
+        newMess = {
+          name: ele.name,
+          lines: [
+            ele.line1,
+            ele.line2,
+            ele.line3,
+            ele.line4,
+            ele.line5,
+            ele.line6,
+            ele.line7,
+            ele.line8,
+            ele.line9,
+            ele.line10,
+            ele.line11,
+            ele.line12,
+            ele.line13,
+            ele.line14,
+            ele.line15,
+            ele.line16,
+            ele.line17,
+            ele.line18,
+            ele.line19,
+            ele.line20,
+          ],
+        };
+
+      for (var i = 1; i <= 9; i++) {
+          if (newMess.name.includes("לינקים לתוצרים " + i)) {
+            messes[i - 1] = newMess;
+          }
+        }
+      });
+      for (var i = 0; i <= 8; i++) {
+        for (var j = 0; j < messes[i].lines.length; j++) {
+            
+          cutMess(messes[i].lines, i + 1);
+        }
+      }
+    });
+}
+function cutMess(linesArr, messType) {
+var crewMem;
+  if (currCrew.name !== "") crewMem = currCrew.name;
+  if (currCrew.name === "") crewMem = "";
+  var currText = "";
+    var testDiv = document.getElementById("text" + messType);
+  if(messType===1||messType===2||messType===7||messType===9){
+  
+        removeAllChildNodes(testDiv);
+    }
+  var i = 0;
+  var firstName2 = firstName;
+  while (linesArr[i] !== "end") {
+    if (linesArr[i].includes("nameOfChain")) {
+      linesArr[i] = linesArr[i].replace(
+        "nameOfChain",
+        document.getElementById("chainName").value
+      );
+    }
+    if (linesArr[i].includes("firstNameOfGuest")) {
+      linesArr[i] = linesArr[i].replace("firstNameOfGuest", firstName2);
+    }
+    if (linesArr[i].includes("fullNameOfGuest")) {
+      var nameAndChain = document
+        .getElementById("peopleList")
+        .value.split(" + ");
+      linesArr[i] = linesArr[i].replace("fullNameOfGuest", nameAndChain[0]);
+    }
+    if (linesArr[i].includes("link555")) {
+      linesArr[i] = linesArr[i].replace("link555", document.getElementById("link555").value);
+    }
+     if (linesArr[i].includes("fullLink")) {
+      linesArr[i] = linesArr[i].replace("fullLink", document.getElementById("full555").value);
+    }
+     if (linesArr[i].includes("linkSpotify")) {
+      linesArr[i] = linesArr[i].replace("linkSpotify", document.getElementById("spotify").value);
+    }
+    if (linesArr[i].includes("link55")) {
+      linesArr[i] = linesArr[i].replace("link55", document.getElementById("short55").value);
+    }
+    if (linesArr[i].includes("linkyoutube55")) {
+      linesArr[i] = linesArr[i].replace("linkyoutube55", document.getElementById("short55yt").value);
+    }
+    if (linesArr[i].includes("crewName")) {
+      linesArr[i] = linesArr[i].replace("crewName", crewMem);
+    }
+    if (linesArr[i] !== "") {
+      if (linesArr[i + 1] !== "end") {
+        currText += linesArr[i] + "\n";
+      }
+      if (linesArr[i + 1] === "end") {
+        currText += linesArr[i];
+      }
+    }
+    if (linesArr[i] === "") {
+      currText += "\n";
+    }
+    var duplicateLine = linesArr[i];
+    while (duplicateLine.includes("*")) {
+      if (duplicateLine.includes("*")) {
+        duplicateLine = duplicateLine.replace("*", "<strong>");
+      }
+      if (duplicateLine.includes("*")) {
+        duplicateLine = duplicateLine.replace("*", "</strong>");
+      }
+    }
+
+    var testH4 = document.createElement("h4");
+    if (linesArr[i] !== "") {
+      if (linesArr[i + 1] === "") {
+        testH4.classList.add("mb-3");
+      }
+      if (linesArr[i + 1] !== "") {
+        testH4.classList.add("mb-0");
+      }
+      testH4.innerHTML = duplicateLine;
+        if(messType===1||messType===2||messType===7||messType===9){
+            testDiv.append(testH4);
+        }
+    }
+    i++;
+  }
+  fullTexts[messType - 1] = currText;
+}
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+setTimeout(() => {
+  const loader = document.getElementById("loader");
+  loader.style.display = "none";
+}, 2050);
+
+function reset() {
+  document.location.reload();
+}
+function checkPhone(phone) {
+  if (wannaFixGuestPhone === true) {
+    if (phone.length === 10 && phone[0] === "0" && phone[1] === "5")
+      return true;
+    return false;
+  } else return true;
+}
+function fixChainFromData(chain) {
+  var splittedChain; //
+  if (chain.includes(" (") || chain.includes("-")) {
+    splittedChain = chain.split(" (");
+    var moresplitted;
+    if (splittedChain[0].includes("-")) {
+      moresplitted = splittedChain[0].split("-");
+      return moresplitted[1].trim();
+    }
+    return splittedChain[0].trim();
+  }
+  return chain;
+}
+function checkInputs() {
+  if (
+    checkPhone(document.getElementById("guestPhone").value) &&
+    document.getElementById("chainsNames").value !== ""
+  ) {
+    return true;
+  }
+  alert("ייתכן שמספר הטלפון אינו תקין!");
+  return false;
+}
+
+function fixChain() {
+  if (document.getElementById("chainName").value !== "") {
+    for (var j = 0; j < allChains.length; j++) {
+      if (
+        document.getElementById("chainName").value === allChains[j].name ||
+        document.getElementById("chainName").value === allChains[j].altName
+      ) {
+        currChain = allChains[j];
+        console.log(currChain);
+      }
+    }
+  }
+  //document.getElementById("nameOfChain2").innerHTML = document.getElementById(  "chainName"  ).value;
+}
+function submit() {
+  toFixGuestPhone();
+  crewChosen();
+  document.getElementById("postMes").style.visibility = "hidden";
+  if (checkInputs()) {
+    fixChain();
+    document.getElementById("postMes").style.visibility = "visible";
+    getMessData();
+  }
+}
+function crewChosen() {
+   if (document.getElementById("crewList").value !== "") {
+    for (var j = 0; j < crewList.length; j++) {
+      if (document.getElementById("crewList").value === crewList[j].name) {
+        currCrew = crewList[j];
+      }
+    }
+  } else {
+    currCrew.name = "";
+    currCrew.phone = "";
+  }
+}
+
+function copy(id) {
+   var text = fullTexts[id - 1];
+  var elem = document.createElement("textarea");
+  document.body.appendChild(elem);
+  elem.value = text;
+  elem.select();
+  document.execCommand("copy");
+  document.body.removeChild(elem);
+  alert("הטקסט הועתק!");
+}
+function phoneForWA(phone) {
+  if (wannaFixGuestPhone === true) {
+    return "972" + phone.slice(1);
+  }
+  return phone;
+}
+function whatsAppMes(id) {
+  var phone = document.getElementById("guestPhone").value;
+  var link =
+    "https://api.whatsapp.com/send?phone=" +
+    phoneForWA(phone) +
+    "&text=" +
+    encodeURI(fullTexts[id - 1]);
+  window.open(link, "_blank");
+}
+function fixPhoneData(phone) {
+  if (wannaFixGuestPhone === true) {
+    if (phone.includes("-")) {
+      console.log("in");
+      phone = phone.replace("-", "");
+    }
+    if (phone.includes(" ")) {
+      phone = phone.replace(" ", "");
+    }
+  }
+  return phone;
+}
+function fixFirstName(phoneNum) {
+  var fullName = "";
+  for (var i = 0; i < size; i++) {
+    if (allPeople[i].phone === phoneNum) fullName = allPeople[i].name;
+  }
+  const splittedName = fullName.split(" ");
+  if (
+    splittedName[0] === 'ד"ר' ||
+    splittedName[0] === "ד״ר" ||
+    splittedName[0] === "דוקטור" ||
+    splittedName[0] === "פרופסור" ||
+    splittedName[0] === "פרופ'" ||
+    splittedName[0] === "Dr."||
+     splittedName[0] === "הרב" ||
+     splittedName[0] === "ד״ר" 
+  ) {
+    return splittedName[1];
+  }
+  return splittedName[0];
+}
+function submitData() {
+  toFixGuestPhone();
+  for (var i = 0; i < allPeople.length; i++) {
+    var nameAndChain = document.getElementById("peopleList").value.split(" + ");
+    if (
+      allPeople[i].name === nameAndChain[0] &&
+      fixChainFromData(allPeople[i].chain) === nameAndChain[1]
+    ) {
+      console.log("row num:" + allPeople[i].row);
+      document.getElementById("chainName").value = fixChainFromData(
+        allPeople[i].chain
+      );
+      fullName = allPeople[i].name;
+      document.getElementById("guestPhone").value = fixPhoneData(
+        allPeople[i].phone
+      );
+      firstName = fixFirstName(allPeople[i].phone);
+      // document.getElementById("nameOfPerson").innerHTML = " " + firstName;
+      document.getElementById("link555").value = allPeople[i].linkfive;
+      document.getElementById("short55").value = allPeople[i].linkshort;
+      document.getElementById("short55yt").value = allPeople[i].linkshortyt;
+      document.getElementById("full555").value = allPeople[i].linkfull;
+      document.getElementById("spotify").value = allPeople[i].linkspotify;
+    }
+  }
+}
+function toFixGuestPhone() {
+  if (document.getElementById("fixGuestPhone").checked === true) {
+    wannaFixGuestPhone = true;
+  } else wannaFixGuestPhone = false;
+}
 
