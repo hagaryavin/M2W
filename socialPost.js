eed2b544aa6d123bbe22b8fd67bec09ce231250b
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-11-21 17:37:54 +0200

    Update socialPost.js

--- socialPost.js
+++ socialPost.js
@@ -535,7 +535,9 @@ function fixFirstName(phoneNum) {
     splittedName[0] === "פרופ'" ||
     splittedName[0] === "Dr."||
      splittedName[0] === "הרב" ||
-     splittedName[0] === "ד״ר" 
+     splittedName[0] === "ד״ר" ||
+     splittedName[0] === 'עו"ד'||
+      splittedName[0] === 'עו״ד'
   ) {
     return splittedName[1];
   }
@@ -553,7 +555,9 @@ function fixCreatorFirstName() {
     splittedName[0] === "פרופ'" ||
     splittedName[0] === "Dr."||
      splittedName[0] === "הרב" ||
-     splittedName[0] === "ד״ר" 
+     splittedName[0] === "ד״ר" ||
+     splittedName[0] === 'עו"ד'||
+      splittedName[0] === 'עו״ד'
   ) {
     return splittedName[1];
   }
@@ -581,7 +585,9 @@ function fixInterviewerFirstName(phoneNum) {
     splittedName[0] === "פרופ'" ||
     splittedName[0] === "Dr."||
      splittedName[0] === "הרב" ||
-     splittedName[0] === "ד״ר" 
+     splittedName[0] === "ד״ר" ||
+     splittedName[0] === 'עו"ד'||
+      splittedName[0] === 'עו״ד'
   ) {
     return splittedName[1];
   }
