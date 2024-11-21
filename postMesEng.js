543dc9f65e2460c80c591b287de169ac0e950693
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-11-21 17:39:44 +0200

    Update postMesEng.js

--- postMesEng.js
+++ postMesEng.js
@@ -378,7 +378,9 @@ function fixFirstName(phoneNum) {
     splittedName[0] === "פרופ'" ||
     splittedName[0] === "Dr."||
      splittedName[0] === "הרב" ||
-     splittedName[0] === "ד״ר" 
+     splittedName[0] === "ד״ר" ||
+     splittedName[0] === 'עו"ד'||
+      splittedName[0] === 'עו״ד'
   ) {
     return splittedName[1];
   }
