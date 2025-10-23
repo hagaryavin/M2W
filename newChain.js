e602b785c199c0558ae95a6a7675f23a25f37fb9
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2025-10-23 12:48:16 +0300

    Add files via upload

--- newChain.js
+++ newChain.js
@@ -15,9 +15,10 @@ var currCrew = "";
 var newCrewMem;
 var messes = [
   { name: "", lines: [] },
+    { name: "", lines: [] },
     { name: "", lines: [] }
 ];
-var fullTexts = [[],[]];
+var fullTexts = [[],[],[]];
 const url =
   "https://script.google.com/macros/s/AKfycbw_2VmXLs1pJKLZElcT2Tp0tR6tPVRf4UWKfS22_n-F_DSEI2dF2zrsQrQ6If6P4mEaGg/exec";
 var newPerson = {};
@@ -158,13 +159,13 @@ function getMessData() {
           ],
         };
 
-       for (var i = 1; i <=2 ; i++) {
-          if (newMess.name.includes("שרשרת חדשה " + i)) {
+       for (var i = 1; i <=3 ; i++) {
+          if (newMess.name.includes("שרשרת.קהילה חדשה " + i)) {
             messes[i - 1] = newMess;
           }
         }
       });
-      for (var i = 0; i <=1; i++) {
+      for (var i = 0; i <=2; i++) {
         for (var j = 0; j < messes[i].lines.length; j++) {
             
           cutMess(messes[i].lines, i + 1);
