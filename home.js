8c515b1ad2c79860707d2258a65d9dee9e22307a
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2026-09-18 11:05:15 +0300

    Add files via upload

--- home.js
+++ home.js
@@ -180,7 +180,7 @@ function getData(x) {
                 if(ele.recordingdate === ""&&ele.fixedrecordingdate === ""&&newPerson.livechain===true){
                     newPerson.recordingdate=addDate(newPerson);
                  }
-                if(ele.recordingdate === ""&&ele.fixedrecordingdate === ""&&(newPerson.chain.includes("כנס.טראומה.וריפוי")||newPerson.chain.includes("כנס.צופים"))){
+                if(ele.recordingdate === ""&&ele.fixedrecordingdate === ""&&(newPerson.chain.includes("כנס.עתידנות")||newPerson.chain.includes("כנס.צופים"))){
                     newPerson.recordingdate=addDateConference(newPerson);
                  }
                 if(ele.name==="הגר יבין"){
